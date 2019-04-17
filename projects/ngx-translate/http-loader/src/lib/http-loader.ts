d9cbb2e6bdb6fcb93d4b5b6592e66efa89b56d1b
Nagendra Kumar 2 <nagkumar@publicisgroupe.net>
2019-04-17 09:34:42 +0530

    [Multiple Translate files loader

--- projects/ngx-translate/http-loader/src/lib/http-loader.ts
+++ projects/ngx-translate/http-loader/src/lib/http-loader.ts
@@ -1,6 +1,11 @@
 import {HttpClient} from "@angular/common/http";
 import {TranslateLoader} from "@ngx-translate/core";
-import {Observable} from 'rxjs';
+import {Observable, of, forkJoin} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+interface TranslateFilesList {
+  prefix: string ; suffix: string;
+}
 
 export class TranslateHttpLoader implements TranslateLoader {
   constructor(private http: HttpClient, public prefix: string = "/assets/i18n/", public suffix: string = ".json") {}
@@ -10,5 +15,20 @@ export class TranslateHttpLoader implements TranslateLoader {
    */
   public getTranslation(lang: string): Observable<Object> {
     return this.http.get(`${this.prefix}${lang}${this.suffix}`);
+  }
+}
+
+export class MultipleTranslateHttpLoader implements TranslateLoader {
+  constructor(private http: HttpClient, public paths:TranslateFilesList[]) {}
+
+  /**
+   * Gets the multiple translations from the server
+   */
+  public getTranslation(lang: string): Observable<Object> {
+
+    return   new Observable(obs=>forkJoin(this.paths.map( item => this.http.get('' + item.prefix + lang +  item.suffix).pipe(catchError(res => {
+      console.error("[Error] Something went wrong with --> ", res.url);
+      return of({});
+    })) ) ).subscribe( res=> { obs.next( Object.assign({}, ...res))} ));
   }
 }
